7b3618b26a2d142fb5834c78f9e2d2f8b98a1513
Elnur <egarav88@mail.ru>
2023-03-02 16:58:14 +0400

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -7,6 +7,10 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -20,6 +24,7 @@ public class Grabber implements Grab {
     private final Scheduler scheduler;
     private final int time;
 
+
     public Grabber(Parse parse, Store store,
                    Scheduler scheduler, int time) {
         this.parse = parse;
@@ -28,7 +33,7 @@ public class Grabber implements Grab {
         this.time = time;
     }
 
-    @Override
+
     public void init() throws SchedulerException {
         JobDataMap data = new JobDataMap();
         data.put("store", store);
@@ -59,6 +64,34 @@ public class Grabber implements Grab {
         }
     }
 
+
+    public void web(Store store) {
+        Properties cfg = new Properties();
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+
     public static void main(String[] args) throws Exception {
         var cfg = new Properties();
         try (InputStream in = Grabber.class.getClassLoader()
@@ -70,6 +103,8 @@ public class Grabber implements Grab {
         var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         var store = new PsqlStore(cfg);
         var time = Integer.parseInt(cfg.getProperty("time"));
-        new Grabber(parse, store, scheduler, time).init();
+        Grabber grab = new Grabber(parse, store, scheduler, time);
+        grab.init();
+        grab.web(store);
     }
 }
